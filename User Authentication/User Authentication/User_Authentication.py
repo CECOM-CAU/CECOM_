2f9764542af8857624282f4673335819d0d6444a
Heo Jun Ku <lmkhkm@naver.com>
2019-07-25 00:27:13 +0900

    테이블 불러오는 코드 완성

--- User Authentication/User Authentication/User_Authentication.py
+++ User Authentication/User Authentication/User_Authentication.py
@@ -3,8 +3,20 @@ import sqlite3 as lite
 conn = lite.connect('test.db')
 curs = conn.cursor()
 
+curs.execute("DROP TABLE IF EXISTS User")
+curs.execute("CREATE TABLE User('UserName', 'Student_ID')")
+curs.execute("INSERT INTO User VALUES('허준구', '20164352')")
 
-curs.fetchall()
+curs.execute("SELECT * FROM User")
+
+sqlData = curs.fetchall()
+print(sqlData)
+
+
+
+
+
 conn.commit()
 curs.close()
 conn.close()
+
