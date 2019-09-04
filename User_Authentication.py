6d7e6596dd88c0fc2a1d0d43f29bfbf968ec914b
splo2t <yu990410@naver.com>
2019-09-05 05:06:26 +0900

    Merge branch 'master' of https://github.com/CECOM-CAU/CECOM_DoorLock
    # Conflicts:
    #	app.py

--- User_Authentication.py
+++ User_Authentication.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect("test.db")
 curs = conn.cursor()
 curs.execute("DROP TABLE IF EXISTS User")
-curs.execute("CREATE TABLE if not exists Iser(Student_ID, Name);")
+curs.execute("CREATE TABLE if not exists User(Student_ID, Name);")
 
 def Authentication(id):
     conn = sqlite3.connect('test.db')
