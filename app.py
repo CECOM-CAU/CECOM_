707b0f9897f3773772fecf2277eb97032ce147bd
splo2t <yu990410@naver.com>
2019-09-04 20:24:47 +0100

    complete test with arduino

--- app.py
+++ app.py
@@ -7,27 +7,19 @@ import socket
 app = Flask(__name__)
 app.secret_key = 'We are Fried Chicken Dinner!!!!'
 
-@app.route("/doorLock", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def doorLock():
     if request.method == 'POST':
-        getJson = request.get_json(silent=True, cache=False, force=True)
-        if getJson:
-            returnjson = json.dumps(getJson)
-        else:
-            returnjson = "no json"
-        print(returnjson)
-        if returnjson[0] == 'q':
-            return User_Authentication.Authentication(returnjson[1:])
-        if returnjson[0] == 'w:':
-            return cecom_doorlock.userRegister(returnjson[1:])
-        if returnjson[0] == 'e':
-            return cecom_doorlock.userDelete(returnjson[1:])
+        returnjson = request.get_json(silent=True, cache=False, force=True)
+        if 'test1' in returnjson:
+            print(returnjson["test1"])
+        return str(returnjson)
     if request.method == 'GET':
         return 'test'
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/communication", methods=['GET', 'POST'])
 def main():
     return "hello world"
 
 if __name__ == '__main__':
     IP = str(socket.gethostbyname(socket.gethostname()))
-    app.run(host=IP, port=5010, debug=True)
+    app.run(host="192.168.0.20", port=9090, debug=True)
