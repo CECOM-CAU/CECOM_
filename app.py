73cbf8c736c5c71ed0955ae6a0487198fb86ecef
splo2t <yu990410@naver.com>
2019-09-05 05:26:02 +0900

    add log

--- app.py
+++ app.py
@@ -28,11 +28,23 @@ def DBinit():
     curs = conn.cursor()
     curs.execute("DROP TABLE IF EXISTS User")
     curs.execute("CREATE TABLE if not exists User(Student_ID, Name);")
+    conn.commit()
+    curs.close()
+    conn.close()
 
     conn = sqlite3.connect("fileLogDB.db")
     curs = conn.cursor()
     curs.execute("CREATE TABLE  if not exists fileLogDB(username, time, file)")
     conn.commit()
+    curs.close()
+    conn.close()
+
+    conn = sqlite3.connect("test.db")
+    curs = conn.cursor()
+    curs.execute("select*from User")
+    curs.execute("INSERT INTO user(Student_ID,Name) VALUES(" + "20178999" + "," + "testdata" + ")")
+    conn.commit()
+    curs.close()
     conn.close()
 
 if __name__ == '__main__':
